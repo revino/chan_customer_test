78406146d335d48f41365a66e3b44ec1804afa21
woong <k632ht@gmail.com>
2022-06-12 23:00:01 +0900

    name change

--- src/main/java/com/order/chanOrderSample/ChanCustomerSampleApplication.java
+++ src/main/java/com/order/chanOrderSample/ChanCustomerSampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChanOrerSampleApplication {
+public class ChanCustomerSampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChanOrerSampleApplication.class, args);
+        SpringApplication.run(ChanCustomerSampleApplication.class, args);
     }
 
 }
