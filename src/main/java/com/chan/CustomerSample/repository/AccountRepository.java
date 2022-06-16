947526c51e489f3cfa7111fe8785682c45607007
woong <k632ht@gmail.com>
2022-06-16 19:20:49 +0900

    teest

--- src/main/java/com/chan/CustomerSample/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chan.CustomerSample.repository;
-
-import com.chan.CustomerSample.domain.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-     Account findByName(String name);
-}
